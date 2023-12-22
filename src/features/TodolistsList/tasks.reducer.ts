8f1ea0e010d3217c681a10b15310a1819ccf846f
Dmitriy <fstpain@gmail.com>
2023-12-22 21:51:12 +0300

    ref

--- src/features/TodolistsList/tasks.reducer.ts
+++ src/features/TodolistsList/tasks.reducer.ts
@@ -1,10 +1,12 @@
 import {TaskPriorities, TaskStatuses, TaskType, todolistsAPI, UpdateTaskModelType} from "api/todolists-api";
-import {AppThunk} from "app/store";
+import {AppDispatch, AppRootStateType, AppThunk} from "app/store";
 import {handleServerAppError, handleServerNetworkError} from "utils/error-utils";
 import {appActions} from "app/app.reducer";
 import {todolistsActions} from "features/TodolistsList/todolists.reducer";
 import {createAsyncThunk, createSlice, PayloadAction} from "@reduxjs/toolkit";
 import {clearTasksAndTodolists} from "common/actions/common.actions";
+import {Dispatch} from "redux";
+import {createAppAsyncThunk} from "utils/createAppAsyncThunk";
 
 const initialState: TasksStateType = {};
 
@@ -70,7 +72,17 @@ export const _fetchTasksTC =
                 return dispatch(appActions.setAppStatus({status: "succeeded"}));
             });
         };
-export const fetchTasks = createAsyncThunk(`${slice.name}/fetchTasks`, async (todolistId: string, thunkAPI) => {
+type AsyncThunkConfig = {
+    state?: unknown
+    dispatch?: Dispatch
+    extra?: unknown
+    rejectValue?: unknown
+    serializedErrorType?: unknown
+    pendingMeta?: unknown
+    fulfilledMeta?: unknown
+    rejectedMeta?: unknown
+}
+export const fetchTasks = createAppAsyncThunk<{ tasks: TaskType[], todolistId: string }, string >(`${slice.name}/fetchTasks`, async (todolistId, thunkAPI) => {
     const {dispatch, rejectWithValue} = thunkAPI
     try {
         dispatch(appActions.setAppStatus({status: "loading"}));
